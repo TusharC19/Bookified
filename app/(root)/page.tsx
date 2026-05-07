5d4bcbf28462acd1afc0fa322e06ec57730d3fbe
Tushar Chaturvedi <tusharchaturvedi1909unova@gmail.com>
2026-05-07 23:31:47 +0530

    do the DB connection but error came

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import HeroSection from "@/components/HeroSection";
 import BookCard from "@/components/BookCard";
 import { sampleBooks } from '@/lib/constants';
-// import {getAllBooks} from "@/lib/actions/book.actions";
+import {getAllBooks} from "@/lib/actions/book.actions";
 // import Search from "@/components/Search";
 
 const Page = async ({ searchParams }: { searchParams: Promise<{ query?: string }> }) => {
     const { query } = await searchParams;
 
-    // const bookResults = await getAllBooks(query)
-    // const books = bookResults.success ? bookResults.data ?? [] : []
+    const bookResults = await getAllBooks(query)
+    const books = bookResults.success ? bookResults.data ?? [] : []
 
     return (
         <main className="wrapper container">
